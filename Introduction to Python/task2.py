843ffe97292241dc461e28b5a25dee049232d3cc
Deonis12 <yevenkodenis@gmail.com>
2022-11-06 15:34:31 +0200

    introduction

---
+++ Introduction to Python/task2.py
@@ -0,0 +1,5 @@
+print('Task 1\n')
+print('To be\nor not\nto be\n')
+
+print('Task 2\n')
+print('"Life is what happens\n', 'when\n'.rjust(9, ' '), ''.ljust(7, ' '),'you`re busy making other plans"\n',''.ljust(35, ' '),'John Lenon')
---
+++ Introduction to Python/task3.py
@@ -0,0 +1,4 @@
+a = input('Input number one: ')
+b = input('Input number two: ')
+c = input('Input number three: ')
+print('My number: ',a+b+c)
